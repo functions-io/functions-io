43b75e21e90ba2f0b22275fdfcbeaeac3245f47f
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2016-11-16 20:53:59 -0200

    openapi  + post

--- sys/openapi.js
+++ sys/openapi.js
@@ -30,59 +30,104 @@ function getSpec(){
             var keysInput;
             var keysOutput;
             var newDefinition;
-            var itemNewDefinition = {};
+            var itemNewDefinitionInput = {};
+            var itemNewDefinitionOutput = {};
             var openapi_get = {};
+            var openapi_post = {};
 
             if (itemFunctionManager.module.category){
                 openapi_get.tags = [itemFunctionManager.module.category]
+                openapi_post.tags = [itemFunctionManager.module.category] 
             }
             if (itemFunctionManager.module.summary){
                 openapi_get.summary = itemFunctionManager.module.summary;
+                openapi_post.summary = itemFunctionManager.module.summary;
             }
             if (itemFunctionManager.module.description){
                 openapi_get.description = itemFunctionManager.module.description;
+                openapi_post.description = itemFunctionManager.module.description;
             }
             openapi_get.consumes = ["application/json"];
             openapi_get.produces = ["application/json"];
+            openapi_post.consumes = ["application/json"];
+            openapi_post.produces = ["application/json"];
 
             //parameters
             openapi_get.parameters = [];
+            openapi_post.parameters = [];
             if (itemFunctionManager.module.input){
                 keysInput = Object.keys(itemFunctionManager.module.input);
-                for (var i_input = 0; i_input < keysInput.length; i_input++){
-                    (function(parameters, itemInput, inputName){
-                        var itemNewParam = {};
-                        itemNewParam.in = "query";
-                        itemNewParam.name = inputName;
-                        itemNewParam.type = itemInput.type;
-                        if (itemInput.required){
-                            itemNewParam.required = itemInput.required;
-                        }
-                        if (itemInput.format){
-                            itemNewParam.format = itemInput.format;
-                        }
-                        if (itemInput.description){
-                            itemNewParam.description = itemInput.description;
-                        }
-                        parameters.push(itemNewParam);
-                    })(openapi_get.parameters, itemFunctionManager.module.input[keysInput[i_input]], keysInput[i_input]);
-                }
-            }
 
+                itemNewDefinitionInput = {};
+                itemNewDefinitionInput.type = "object";
+                itemNewDefinitionInput.required = [];
+                itemNewDefinitionInput.properties = {};
+
+                openapi_post.parameters.push({"name": "in_" + itemFunctionManager.key, "in": "body", "required": true, "schema": {"$ref": "#/definitions/in_" + itemFunctionManager.key}});
+                for (var i_input = 0; i_input < keysInput.length; i_input++){
+                    (function(properties, parameters_get, itemInput, inputName){
+                        var itemNewParam_get = {};
+                        var itemNewProperty = {};
+                        
+                        itemNewParam_get.in = "query";
+                        itemNewParam_get.name = inputName;
+                        
+                        itemNewParam_get.type = itemInput.type;
+                        itemNewProperty.type = itemInput.type;
+
+                        if (itemInput.required){
+                            itemNewParam_get.required = itemInput.required;
+                            itemNewDefinitionInput.required.push(inputName);
+                        }
+
+                        if (itemInput.format){
+                            itemNewParam_get.format = itemInput.format;
+                            itemNewProperty.format = itemInput.format;
+                        }
+
+                        if (itemInput.description){
+                            itemNewParam_get.description = itemInput.description;
+                            itemNewProperty.description = itemInput.description;
+                        }
+
+                        parameters_get.push(itemNewParam_get);
+                        properties[inputName] = itemNewProperty;
+                    })(itemNewDefinitionInput.properties, openapi_get.parameters, itemFunctionManager.module.input[keysInput[i_input]], keysInput[i_input]);
+                }
+                specOpenApi.definitions["in_" + itemFunctionManager.key] = itemNewDefinitionInput;
+            }
 
             openapi_get.responses = {};
             openapi_get.responses[200] = {};
             openapi_get.responses[200].description = "Successful response";
+
+            openapi_post.responses = {};
+            openapi_post.responses[200] = {};
+            openapi_post.responses[200].description = "Successful response";
             
             if (itemFunctionManager.module.output){
                 keysOutput = Object.keys(itemFunctionManager.module.output);
 
                 openapi_get.responses[200].schema = {};
-                openapi_get.responses[200].schema["$ref"] = "#/definitions/" + itemFunctionManager.key;    
-                itemNewDefinition = {};
-                itemNewDefinition.type = "object";
-                itemNewDefinition.required = [];
-                itemNewDefinition.properties = {};
+                openapi_get.responses[200].schema["$ref"] = "#/definitions/out_msg_" + itemFunctionManager.key;    
+                openapi_post.responses[200].schema = {};
+                openapi_post.responses[200].schema["$ref"] = "#/definitions/out_msg_" + itemFunctionManager.key;
+                
+                (function(){
+                    var itemNewDefinitionOutput = {};
+                    itemNewDefinitionOutput.type = "object";
+                    itemNewDefinitionOutput.properties = {};
+                    itemNewDefinitionOutput.properties["id"] = {type: "integer", description: "correlation id"};
+                    itemNewDefinitionOutput.properties["result"] = {"$ref": "out_" + itemFunctionManager.key};
+                    itemNewDefinitionOutput.properties["error"] = {};
+
+                    specOpenApi.definitions["out_msg_" + itemFunctionManager.key] = itemNewDefinitionOutput;
+                })();
+
+                itemNewDefinitionOutput = {};
+                itemNewDefinitionOutput.type = "object";
+                itemNewDefinitionOutput.required = [];
+                itemNewDefinitionOutput.properties = {};
                 
                 for (var i_output = 0; i_output < keysOutput.length; i_output++){
                     (function(properties, itemOutput, outputName){
@@ -95,17 +140,17 @@ function getSpec(){
                             itemNewProperty.description = itemOutput.description;    
                         }
                         if (itemOutput.required){
-                            itemNewDefinition.required.push(outputName);
+                            itemNewDefinitionOutput.required.push(outputName);
                         }
                         /*"Pets": {"type": "array","items": {"$ref": "#/definitions/Pet"}},*/
                         
                         properties[outputName] = itemNewProperty;
-                    })(itemNewDefinition.properties, itemFunctionManager.module.output[keysOutput[i_output]], keysOutput[i_output]);
+                    })(itemNewDefinitionOutput.properties, itemFunctionManager.module.output[keysOutput[i_output]], keysOutput[i_output]);
                 }
-                specOpenApi.definitions[itemFunctionManager.key] = itemNewDefinition; 
+                specOpenApi.definitions["out_" + itemFunctionManager.key] = itemNewDefinitionOutput; 
             }
 
-            specOpenApi.paths["/" + itemFunctionManager.name + "/" + itemFunctionManager.version] = {get: openapi_get};
+            specOpenApi.paths["/" + itemFunctionManager.name + "/" + itemFunctionManager.version] = {get: openapi_get, post: openapi_post};
         })(module._factory.listFunctionManager[keys[i]]);
     }
 
