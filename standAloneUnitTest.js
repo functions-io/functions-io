d02ebf55d919d38d53523fd98bfb788c1585e80c
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2016-12-04 20:29:59 -0200

    refactor

--- standAloneUnitTest.js
+++ standAloneUnitTest.js
@@ -1,18 +1,18 @@
 var http = require("http");
-var fs = require("fs");
 var args = process.argv.slice(2);
 var file = args[0];
-var dirBase = "test/functions";
+var dirBase = "functions";
 var name = "";
 var path = "";
 
-console.log(file);
-
 name = file.substring(dirBase.length);
-name = name.substring(0, name.length - 3);
+name = name.substring(1, name.length - 3);
+name = name.replace(/\//g,".");
+
+path = "/" + name + "/?stage=_unitTest";
 
 function httpPost(path, callBack){
-    var options = {host: "localhost", port: "8080", path: path, method: "POST", headers: {"Content-Type": "application/json"}};
+    var options = {host: "127.0.0.1", port: "8080", path: path, method: "POST", headers: {"Content-Type": "application/json"}};
     
     var req = http.request(options, function(res) {
         var data = "";
@@ -28,34 +28,28 @@ function httpPost(path, callBack){
                 callBack(res.statusCode, JSON.parse(data));
             }
         });
+        res.on("error", function(e) {
+            console.log("problem with request: " + e);
+        });
     });
     
     req.end();
 }
 
-fs.readFile(file, function(err, code){
-    if (err){
-        console.error(err);
+httpPost(path, function(errHTTP, dataHTTP){
+    if (errHTTP){
+        console.log(errHTTP);
     }
     else{
-        path = name + "?stage=_unitTest";
-        
-        httpPost(path, function(errHTTP, dataHTTP){
-            if (errHTTP){
-                console.log(errHTTP);
-            }
-            else{
-                console.log(dataHTTP);
-                if (dataHTTP.result.success){
-                    console.log("unit test - OK");
-                }
-                else{
-                    console.log("unit test - FAIL");
-                }
-                for (var i = 0; i < dataHTTP.result.listResult.length; i++){
-                    console.log(dataHTTP.result.listResult[i]);
-                }
-            }
-        })
+        if (dataHTTP.result.success){
+            console.log("unit test - OK");
+        }
+        else{
+            console.log("unit test - FAIL");
+        }
+        for (var i = 0; i < dataHTTP.result.listResult.length; i++){
+            var item = dataHTTP.result.listResult[i];
+            console.log(item.success + " - " + item.description + " - " + item.time);
+        }
     }
 });
