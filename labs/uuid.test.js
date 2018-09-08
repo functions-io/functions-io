616e745770642cf5b357ac4279451ec91a4fb101
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-09-07 22:30:35 -0300

    init

--- labs/uuid.test.js
+++ labs/uuid.test.js
@@ -3,7 +3,7 @@ const functionsio = require("../lib");
 var message = {};
 message.id = 1;
 message.jsonrpc = "2.0";
-message.method = "uuid/v44";
+message.method = "uuid/v4";
 message.params = [null, null, null];
 
 functionsio.invokeFactory.invokeMessage(message, function(errInvoke, messageResponse){
